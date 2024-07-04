228e132bdb0558fcad542489a2d571d7d56ae411
Frainer Encarnación <fraineralex2001@gmail.com>
2024-07-03 22:47:01 -0400

    feat: :sparkles: set up authentication with Clerk

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,16 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+	plugins: ['prettier-plugin-tailwindcss'],
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid',
+	tabWidth: 2,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: true,
+	useTabs: true
+}
 
-export default config;
+export default config
