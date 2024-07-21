cff48bb78f1ad3632badfef797dc0bccb00a8a2f
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-07-21 18:43:35 -0400

    feat: :heavy_plus_sign: add sonner to display toas messages and rename logos.tsx to icons.tsx

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -55,15 +55,32 @@ export const T3Logo = (props: React.SVGProps<SVGSVGElement>) => (
 
 export function HouseIcon(props: React.SVGProps<SVGSVGElement>) {
 	return (
-		<svg
-			xmlns='http://www.w3.org/2000/svg'
-			viewBox='0 0 576 512'
-			{...props}
-		>
+		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512' {...props}>
 			<path
 				fill='currentColor'
 				d='M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40h-16c-1.1 0-2.2 0-3.3-.1c-1.4.1-2.8.1-4.2.1H392c-22.1 0-40-17.9-40-40v-88c0-17.7-14.3-32-32-32h-64c-17.7 0-32 14.3-32 32v88c0 22.1-17.9 40-40 40h-55.9c-1.5 0-3-.1-4.5-.2c-1.2.1-2.4.2-3.6.2h-16c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9.1-2.8v-69.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7l255.4 224.5c8 7 12 15 11 24z'
 			></path>
+		</svg>
+	)
+}
+
+export function LoadingSpinnerSVG() {
+	return (
+		<svg
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			xmlns='http://www.w3.org/2000/svg'
+			fill='white'
+		>
+			<path
+				d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z'
+				opacity='.25'
+			/>
+			<path
+				d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z'
+				className='spinner_ajPY'
+			/>
 		</svg>
 	)
 }
