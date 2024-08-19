8184ac73263afb32d24d01c710f1eba60d80a69e
Frainer Encarnación <fraineralex2001@gmail.com>
2024-08-18 23:43:14 -0400

    feat: :construction: add some progress to the dashboard page, food registration and exercise registration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,75 @@
+import { Plus } from 'lucide-react'
+import { Button } from '~/components/ui/button'
+import FoodDialog from './_components/food-dialog'
+import ExerciseDialog from './_components/exercise-dialog'
+
 export default function DashboardPage() {
 	return (
-		<section className='w-full pt-16'>
-			<h1>Dashboard</h1>
+		<section className='w-full ps-5 pt-16'>
+			<div className='flex justify-between'>
+				<h1 className='mb-5 text-xl font-semibold uppercase'>
+					{new Date().toLocaleDateString('en-US', {
+						weekday: 'long',
+						month: 'long',
+						day: 'numeric'
+					})}
+				</h1>
+
+				<article className='float-end flex space-x-5'>
+					<FoodDialog />
+					<ExerciseDialog />
+				</article>
+			</div>
+			<article className='h-fit w-fit bg-slate-900/50 p-5'>
+				<h2 className='mb-3'>Nutrition & Targets</h2>
+				<div className='grid grid-flow-row'>
+					<div className='mt-2 flex space-x-5 border-b pb-2'>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+					</div>
+					<div className='mt-2 flex space-x-5 border-b pb-2'>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+					</div>
+					<div className='mt-2 flex space-x-5 border-b pb-2'>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+					</div>
+					<div className='mt-2 flex space-x-5 pb-2'>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+						<span className='rounded-md bg-slate-800 px-4 py-5'></span>
+					</div>
+					<div className='mt-2 flex space-x-10 pb-2'>
+						<span className='ps-2'>T</span>
+						<span className=''>M</span>
+						<span className=''>W</span>
+						<span className=''>T</span>
+						<span className=''>F</span>
+						<span className=''>S</span>
+						<span className='ps-2'>S</span>
+					</div>
+				</div>
+			</article>
 		</section>
 	)
 }
