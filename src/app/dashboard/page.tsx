8906f3f8098b7144f0ff1e5b6a6e54719c21ec37
Frainer Encarnación <fraineralex2001@gmail.com>
2024-08-28 23:55:27 -0400

    feat: :sparkles: add DataAndHabits section for displaying nutrition and habits on the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,10 +2,11 @@ import FoodDialog from './_components/food-dialog'
 import ExerciseDialog from './_components/exercise-dialog'
 import NutritionGraphic from './_sections/nutrition-graphic'
 import InsightsAndAnalitics from './_sections/insights-analytics'
+import DataAndHabits from './_sections/data-habits'
 
 export default function DashboardPage() {
 	return (
-		<section className='w-full ps-5 pt-16'>
+		<section className='w-full ms-5 px-5 pt-16 h-full overflow-auto pb-10'>
 			<div className='flex justify-between'>
 				<h1 className='mb-5 text-xl font-semibold uppercase'>
 					{new Date().toLocaleDateString('en-US', {
@@ -24,6 +25,7 @@ export default function DashboardPage() {
 				<NutritionGraphic />
 				<InsightsAndAnalitics />
 			</div>
+			<DataAndHabits />
 		</section>
 	)
 }
