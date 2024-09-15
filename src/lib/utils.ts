79b472d4bd6c5939ea418963a54f11c1581f150a
Frainer Encarnación <fraineralex2001@gmail.com>
2024-09-14 23:56:39 -0400

    feat: :sparkles: create a function in utils.ts to calculate energy burned based on duration, effort level, weight, age, sex, and height

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,44 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { EFFORT_LEVELS } from '~/constants'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+
+export function calculateEnergyBurned({
+	duration,
+	effort,
+	weight,
+	weightUnit,
+	age,
+	sex,
+	height,
+	heightUnit
+}: {
+	duration: number
+	effort: string
+	weight: number
+	weightUnit: string
+	age: number
+	sex: string
+	height: number
+	heightUnit: string
+}) {
+	weight = weightUnit === 'kg' ? weight : weight * 0.453592
+	if (heightUnit === 'ft') {
+		height = height * 30.48
+	} else if (heightUnit === 'in') {
+		height = height * 2.54
+	}
+  
+	const tmb =
+		sex === 'male'
+			? 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age
+			: 447.593 + 9.247 * weight + 3.098 * height - 4.33 * age
+
+	const caloriesPerMinute =
+		(tmb / 1440) *
+		EFFORT_LEVELS[effort as keyof typeof EFFORT_LEVELS].multiplier
+	return (duration * caloriesPerMinute).toFixed(2)
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-import { LucideProps } from 'lucide-react'
-import { ComponentType } from 'react'
 import { exerciseCategory } from '~/server/db/schema'
 
 export type sex = 'male' | 'female'
