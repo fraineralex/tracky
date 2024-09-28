d148e8049348cc8cd129c5beecf0e06f52fb38a6
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-09-27 23:29:29 -0400

    feat: :sparkles: enhance nutrition graphic with daily data visualization and dynamic styling

--- src/app/dashboard/_sections/nutrition-graphic.tsx
+++ src/app/dashboard/_sections/nutrition-graphic.tsx
@@ -13,6 +13,19 @@ export default function NutritionGraphic({
 }: NutritionMetrics) {
 	const [showConsumed, setShowConsumed] = React.useState(true)
 
+	const dayOfWeek = new Date().getDay()
+	const days = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+	const percentage = 20
+	const nutrition: { [key: number]: number[] } = {
+		0: [10, 40, 80, 90],
+		1: [90, 20, 20, 35],
+		2: [20, 60, 50, 70],
+		3: [50, 30, 90, 20],
+		4: [40, 90, 25, 40],
+		5: [65, 50, 65, 80],
+		6: [30, 80, 30, 50]
+	}
+	const colors = ['#60a5fa', '#fb923c', '#facc15', '#4ade80']
 	return (
 		<article className='h-fit w-full max-w-[490px] rounded-lg border bg-slate-200 p-5 dark:bg-slate-800/50'>
 			<h2 className='mb-3'>Nutrition & Targets</h2>
@@ -22,8 +35,11 @@ export default function NutritionGraphic({
 						<div className='flex space-x-5 border-b pb-2' key={index}>
 							{[...Array(7)].map((_, index2) => (
 								<span
-									className={`rounded-md bg-primary/20 px-4 py-5 ${index2 === 3 ? 'border-2 border-primary' : ''}`}
+									className={`rounded-md px-4 py-4 ${index2 === dayOfWeek - 1 ? 'border-2 border-primary' : ''}`}
 									key={index2}
+									style={{
+										background: `linear-gradient(to top, ${colors[index]} ${nutrition[index2]?.[index]}%, hsl(var(--primary) / 0.2) ${nutrition[index2]?.[index]}%)`
+									}}
 								></span>
 							))}
 						</div>
@@ -57,14 +73,15 @@ export default function NutritionGraphic({
 					</p>
 				</aside>
 			</div>
-			<div className='mt-4 flex space-x-10'>
-				<span className='ps-2'>T</span>
-				<span className=''>M</span>
-				<span className=''>W</span>
-				<span className=''>T</span>
-				<span className=''>F</span>
-				<span className=''>S</span>
-				<span className='ps-2'>S</span>
+			<div className='mt-4 flex space-x-4'>
+				{days.map((day, index) => (
+					<span
+						className={`px-3 py-2 ${index === dayOfWeek - 1 ? 'rounded-md border-2 border-primary font-bold' : ''}`}
+						key={index}
+					>
+						{day}
+					</span>
+				))}
 			</div>
 			<footer className='mt-4 flex place-content-center space-x-3'>
 				<Button
