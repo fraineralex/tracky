ae9b9a377f16ffa8b5adf95eee08b0679719121d
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-09-29 19:11:56 -0400

    feat: :sparkles: update DataAndHabits component to use dynamic date range and pass weight data as props

--- src/app/dashboard/_sections/data-habits.tsx
+++ src/app/dashboard/_sections/data-habits.tsx
@@ -2,15 +2,27 @@ import { Circle, Square } from 'lucide-react'
 import InsightsCard from '../_components/insights-card'
 import ResumeStreak from '../_components/resume-streak'
 
-export default function DataAndHabits() {
+export default function DataAndHabits({
+	weight,
+	weightUnit,
+	updatedAt
+}: {
+	weight: number
+	weightUnit: string
+	updatedAt: string
+}) {
+	const dateRange = `${new Date(updatedAt).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'short'
+	})} - Now`
 	return (
 		<section className='mt-8 flex justify-between'>
 			<InsightsCard
 				title='Nutrition'
-				dateRange='Jul 14 - Now'
+				dateRange={dateRange}
 				value={0}
 				valueUnit='kcal'
-				className='w-full max-w-xs 2xl:max-w-xs rounded-lg dark:bg-slate-800/50 bg-slate-200/50 p-4 pb-1 border'
+				className='w-full max-w-xs rounded-lg border bg-slate-200/50 p-4 pb-1 dark:bg-slate-800/50 2xl:max-w-xs'
 			>
 				<div className='mb-3 mt-3 flex place-content-end'>
 					<Square className='h-4 w-4 text-yellow-400' strokeWidth={4} />
@@ -19,10 +31,10 @@ export default function DataAndHabits() {
 			<ResumeStreak />
 			<InsightsCard
 				title='Scale Weight'
-				dateRange='Jul 14 - Now'
-				value={195.0}
-				valueUnit='lbs'
-				className='w-full max-w-xs 2xl:max-w-xs rounded-lg dark:bg-slate-800/50 bg-slate-200/50 p-4 pb-1 border'
+				dateRange={dateRange}
+				value={weight}
+				valueUnit={weightUnit}
+				className='w-full max-w-xs rounded-lg border bg-slate-200/50 p-4 pb-1 dark:bg-slate-800/50 2xl:max-w-xs'
 			>
 				<div className='mb-3 mt-3 flex place-content-end'>
 					<Circle className='h-4 w-4 text-green-400' strokeWidth={4} />
