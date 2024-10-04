c9167f5969162a8482171329b44580918376f997
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-10-03 23:55:09 -0400

    refactor: :art: update FoodPage component to fetch and display user-specific nutrition metrics and weights

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -1,13 +1,27 @@
 import NutritionGraphic from './sections/nutrition-graphics'
 import { NutritionCards } from './sections/nutrition-cards'
 import { Header } from './sections/header'
+import { currentUser } from '@clerk/nextjs/server'
+import { NutritionMetrics, PublicMetadata } from '~/types'
+import { getUserNutritionMetrics } from '~/server/utils/nutrition'
+import { getAdjustedDay } from '~/lib/utils'
 
-export default function FoodPage() {
+export default async function FoodPage() {
+	const user = await currentUser()
+	if (!user) return null
+	const userMetadata = user.publicMetadata as PublicMetadata
+	const nutritionMeatrics = await getUserNutritionMetrics(user.id, userMetadata)
+	const today = getAdjustedDay(new Date())
+	const todayNutrition = nutritionMeatrics[today] as NutritionMetrics
+
 	return (
 		<div className='container mx-auto px-4 py-8'>
-      <Header />
-			<NutritionCards />
-			<NutritionGraphic />
+			<Header />
+			<NutritionCards nutrition={todayNutrition} />
+			<NutritionGraphic
+				nutritionMeatrics={nutritionMeatrics}
+				weightsChanges={userMetadata.weights}
+			/>
 		</div>
 	)
 }
