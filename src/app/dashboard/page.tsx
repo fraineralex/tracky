4f858ae02b834462389879fca043241d3459b1f8
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-10-03 23:49:28 -0400

    feat: :sparkles: integrate getUserNutritionMetrics for improved nutritional data retrieval in DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,66 +3,19 @@ import ExerciseDialog from './_components/exercise/exercise-dialog'
 import NutritionGraphic from './_sections/nutrition-graphic'
 import InsightsAndAnalitics from './_sections/insights-analytics'
 import DataAndHabits from './_sections/data-habits'
-import { calculateNutritionalNeeds } from '~/lib/utils'
 import { currentUser } from '@clerk/nextjs/server'
-import {
-	NutritionMetricsPerDay,
-	PublicMetadata
-} from '~/types'
-import { db } from '~/server/db'
-import { consumption, food } from '~/server/db/schema'
-import { eq, and, gte } from 'drizzle-orm'
+import { PublicMetadata } from '~/types'
+import { getUserNutritionMetrics } from '~/server/utils/nutrition'
 
 export default async function DashboardPage() {
 	const user = await currentUser()
 	if (!user) return null
 	const userMetadata = user.publicMetadata as PublicMetadata
-	const nutritionMeatrics = calculateNutritionalNeeds(userMetadata)
-	const dayOfWeek = new Date()
-	dayOfWeek.setDate(dayOfWeek.getDate() - dayOfWeek.getDay() - 1)
-	const result = await db
-		.select()
-		.from(consumption)
-		.innerJoin(food, eq(consumption.foodId, food.id))
-		.where(
-			and(
-				eq(consumption.userId, user.id),
-				gte(consumption.createdAt, dayOfWeek)
-			)
-		)
-		
-	const nutritionMeatricsPerDay: NutritionMetricsPerDay = {}
-	Array.from({ length: 7 }).forEach((_, index) => {
-		nutritionMeatricsPerDay[index + 1] = structuredClone(nutritionMeatrics)
-	})
-
-	result.forEach(({ consumption, food }) => {
-		const calories =
-			(Number(consumption.portion) / Number(food.servingSize)) *
-			Number(food.kcal)
-		const protein =
-			(Number(consumption.portion) / Number(food.servingSize)) *
-			Number(food.protein)
-		const carbs =
-			(Number(consumption.portion) / Number(food.servingSize)) *
-			Number(food.carbs)
-		const fats =
-			(Number(consumption.portion) / Number(food.servingSize)) *
-			Number(food.fat)
-
-		const day = consumption.createdAt.getDay()
-		if (!nutritionMeatricsPerDay[day]) {
-			nutritionMeatricsPerDay[day] = structuredClone(nutritionMeatrics)
-		}
-
-		nutritionMeatricsPerDay[day].calories.consumed += calories
-		nutritionMeatricsPerDay[day].protein.consumed += protein
-		nutritionMeatricsPerDay[day].carbs.consumed += carbs
-		nutritionMeatricsPerDay[day].fats.consumed += fats
-	})
+	const nutritionMeatrics = await getUserNutritionMetrics(user.id, userMetadata)
+	const expenditure = nutritionMeatrics[0]?.calories.needed ?? 0
 
 	return (
-		<section className='xl:ms-5 h-full w-full overflow-auto xl:px-5 pb-10 pt-10'>
+		<section className='h-full w-full overflow-auto pb-10 pt-10 xl:ms-5 xl:px-5'>
 			<div className='flex justify-between'>
 				<h1 className='mb-5 text-xl font-semibold uppercase'>
 					{new Date().toLocaleDateString('en-US', {
@@ -77,12 +30,9 @@ export default async function DashboardPage() {
 					<ExerciseDialog />
 				</header>
 			</div>
-			<div className='lg:flex space-x-3 lg:justify-between pt-5 flex-col md:flex-row space-y-3'>
-				<NutritionGraphic nutritionMetrics={nutritionMeatricsPerDay} />
-				<InsightsAndAnalitics
-					expenditure={nutritionMeatrics.calories.needed}
-					{...userMetadata}
-				/>
+			<div className='flex-col space-x-3 space-y-3 pt-5 md:flex-row lg:flex lg:justify-between'>
+				<NutritionGraphic nutritionMetrics={nutritionMeatrics} />
+				<InsightsAndAnalitics expenditure={expenditure} {...userMetadata} />
 			</div>
 			<DataAndHabits {...userMetadata} />
 		</section>
