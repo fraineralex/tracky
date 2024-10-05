c3957c687319cab99fb56ab9d894aef6411f8b7e
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-10-04 23:27:19 -0400

    feat: :sparkles: update ExerciseDialog and FoodDialog components with new icons and button styles for improved UI

--- src/app/dashboard/_components/exercise/exercise-dialog.tsx
+++ src/app/dashboard/_components/exercise/exercise-dialog.tsx
@@ -1,4 +1,4 @@
-import { Plus } from 'lucide-react'
+import { Dumbbell, Plus } from 'lucide-react'
 import { Button } from '~/components/ui/button'
 import {
 	Dialog,
@@ -14,13 +14,12 @@ import { exerciseCategory } from '~/server/db/schema'
 
 export default async function ExerciseDialog() {
 	const categories = await db.select().from(exerciseCategory)
-	
+
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
-				<Button variant='default' className='font-medium'>
-					<Plus className='me-2' />
-					Add Exercise
+				<Button size='sm' className='flex-grow sm:flex-grow-0'>
+					<Dumbbell className='mr-2 h-4 w-4' /> Add Exercise
 				</Button>
 			</DialogTrigger>
 			<DialogContent className='max-w-lg sm:max-w-xl md:max-w-3xl'>
