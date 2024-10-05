c3e8ebb7309c8850bb736a0f2d25af4f63cd442c
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-10-04 23:27:31 -0400

    feat: :sparkles: refactor DataAndHabits component to use userMetadata and calculate nutritional needs

--- src/app/dashboard/_sections/data-habits.tsx
+++ src/app/dashboard/_sections/data-habits.tsx
@@ -1,30 +1,44 @@
 import { Circle, Square } from 'lucide-react'
 import InsightsCard from '../_components/insights-card'
 import ResumeStreak from '../_components/resume-streak'
-import { Weights } from '~/types'
+import { PublicMetadata, Weights } from '~/types'
+import { calculateNutritionalNeeds } from '~/lib/utils'
 
-export default function DataAndHabits({
-	weights,
-	weightUnit,
-	updatedAt
-}: {
-	weights: Weights
-	weightUnit: string
-	updatedAt: string
-}) {
-	const currentWeight = weights[weights.length - 1]?.value ?? 0
-	const dateRange = `${new Date(updatedAt).toLocaleDateString('en-US', {
-		day: 'numeric',
-		month: 'short'
-	})} - Now`
+interface Props {
+	userMetadata: PublicMetadata
+	expenditure: number
+}
+
+export default async function DataAndHabits({
+	userMetadata,
+	expenditure
+}: Props) {
+	const currentWeight =
+		userMetadata.weights[userMetadata.weights.length - 1]?.value ?? 0
+	const dateRange = `${new Date(userMetadata.updatedAt).toLocaleDateString(
+		'en-US',
+		{
+			day: 'numeric',
+			month: 'short'
+		}
+	)} - Now`
+
+	const weights = [userMetadata.weights[0]] as Weights
+	const nutritionMeatrics = await calculateNutritionalNeeds({
+		...userMetadata,
+		weights
+	})
+	const caloriesChanges = Math.abs(
+		nutritionMeatrics.calories.needed - expenditure
+	)
 	return (
-		<section className='mt-3 grid max-w-[460px] grid-cols-2 md:mt-8 md:flex md:max-w-full md:space-x-2 lg:justify-between mx-auto gap-3 md:gap-0'>
+		<section className='mx-auto mt-3 grid max-w-[460px] grid-cols-2 gap-3 md:flex md:max-w-full md:gap-0 md:space-x-2 lg:justify-between'>
 			<InsightsCard
 				title='Nutrition'
 				dateRange={dateRange}
-				value={0}
+				value={caloriesChanges}
 				valueUnit='kcal'
-				className='w-56 rounded-lg border bg-slate-200/50 p-4 pb-1 dark:bg-slate-800/50 md:w-full md:max-w-xs'
+				className='w-56 rounded-lg border p-4 pb-1 dark:bg-slate-800/50 md:w-full md:max-w-xs'
 			>
 				<div className='mb-3 mt-3 flex place-content-end'>
 					<Square className='h-4 w-4 text-yellow-400' strokeWidth={4} />
@@ -35,8 +49,8 @@ export default function DataAndHabits({
 				title='Scale Weight'
 				dateRange={dateRange}
 				value={currentWeight}
-				valueUnit={weightUnit}
-				className='w-56 rounded-lg border bg-slate-200/50 p-4 pb-1 dark:bg-slate-800/50 md:w-full md:max-w-xs'
+				valueUnit={userMetadata.weightUnit}
+				className='w-56 rounded-lg border p-4 pb-1 dark:bg-slate-800/50 md:w-full md:max-w-xs'
 			>
 				<div className='mb-3 mt-3 flex place-content-end'>
 					<Circle className='h-4 w-4 text-green-400' strokeWidth={4} />
