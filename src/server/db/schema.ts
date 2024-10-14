61a1f1f73727e6e8bf024876a24accecbee6b587
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-10-14 17:50:14 -0400

    refactor: :art: add 'uncategorized' as default value for mealGroup field in the schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -56,7 +56,7 @@ export const consumption = createTable(
 			.notNull(),
 		portion: decimal('serving_size', { precision: 7, scale: 2 }).notNull(),
 		unit: unitEnum('unit').notNull(),
-		mealGroup: diaryGroupEnum('diary_group').notNull(),
+		mealGroup: diaryGroupEnum('diary_group').notNull().default('uncategorized'),
 		createdAt: timestamp('created_at', { withTimezone: true })
 			.default(sql`CURRENT_TIMESTAMP`)
 			.notNull(),
@@ -112,7 +112,7 @@ export const exercise = createTable(
 		categoryId: uuid('category_id')
 			.references(() => exerciseCategory.id)
 			.notNull(),
-		diaryGroup: diaryGroupEnum('diary_group').notNull(),
+		diaryGroup: diaryGroupEnum('diary_group').notNull().default('uncategorized'),
 		userId: varchar('user_id', { length: 50 }).notNull(),
 		createdAt: timestamp('created_at', { withTimezone: true })
 			.default(sql`CURRENT_TIMESTAMP`)
