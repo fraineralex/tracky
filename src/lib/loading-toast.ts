fc2d0302194480c420ffba8584a410a25a9be824
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-11-21 12:34:56 -0400

    feat: ✨ add loading toast utility for enhanced user feedback during asynchronous operations

---
+++ src/lib/loading-toast.ts
@@ -0,0 +1,15 @@
+import { toast } from 'sonner'
+
+export function loadingToast(loading: string, id: string) {
+	const promise = () =>
+		new Promise(resolve =>
+			setTimeout(() => resolve({ name: 'Sonner' }), 100000)
+		)
+
+	toast.promise(promise, {
+		loading,
+		id
+	})
+
+  return () => toast.dismiss(id)
+}
