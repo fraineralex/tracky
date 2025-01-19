9c862afd92ab218a5b7ee6eb41f8fcf1303e52d7
Frainer Encarnacion <fraineralex2001@gmail.com>
2025-01-18 22:40:18 -0400

    feat: ✨ enable experimental React compiler in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ await import('./src/env.js')
 /** @type {import("next").NextConfig} */
 const config = {
 	experimental: {
+		reactCompiler: true,
 		ppr: 'incremental',
 		dynamicIO: true,
 		staleTimes: {
